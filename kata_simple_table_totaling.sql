1493bd2e2fb32b109d8ac12b390f0821a986c1ff
Margarita Montenegro <margamontv@gmail.com>
2023-04-29 09:51:52 +0200

    kata_simple_table_totaling

---
+++ kata_simple_table_totaling.sql
@@ -0,0 +1,11 @@
+SELECT
+  RANK() OVER(ORDER BY SUM(points) DESC) AS rank,
+  CASE
+    WHEN clan = '' THEN '[no clan specified]'
+    ELSE clan
+  END as clan,
+  SUM(points) AS total_points,
+  COUNT(clan) AS total_people
+FROM people
+GROUP BY 2
+ORDER BY 3 DESC
